62c429354c59afcc490117f43afc547564772276
ivan.voronchenko <voronchenkoin@profdstu.ru>
2024-04-23 22:09:12 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import WebApp from '@twa-dev/sdk';
+
+
 
 function App() {
+
+  useEffect(()=>{
+    WebApp.ready();
+  },[]);
+
+  const onClose = () => {
+      WebApp.close()
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
